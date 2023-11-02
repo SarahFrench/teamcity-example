2cc75eedb559a9c4a7f5b52ecdbfda239ee443ff
Sarah French <sarah.french@hashicorp.com>
2023-11-02 20:10:15 +0000

    Add second packageA test

--- internal/services/packageA/package_A_test.go
+++ internal/services/packageA/package_A_test.go
@@ -9,3 +9,11 @@ func TestAcc_HelloWorld_PackageA(t *testing.T) {
 		t.Fatalf("wanted `%s`, got `%s`", want, got)
 	}
 }
+
+func TestAcc_HelloWorld_PackageA_copy(t *testing.T) {
+	want := "Hello from Package A"
+	got := HelloWorld_PackageA()
+	if want != got {
+		t.Fatalf("wanted `%s`, got `%s`", want, got)
+	}
+}
